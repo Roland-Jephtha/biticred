e80555610e5752d521aec6ba9690dffa69bf502e
Jephtha <jephtroland832@gmail.com>
2025-09-11 13:26:50 +0100

    App

--- track/admin.py
+++ track/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 # Register your models here.
 
 
 admin.site.register(Shipment)
 admin.site.register(Contact)
 admin.site.register(Phone)
+admin.site.unregister(Group)
 
 
-
-admin.site.site_header = "Metro Express Parcel Service"
-admin.site.index_title = "Manage Metro Express Parcel Service"
+admin.site.site_header = "BitiCred Speed Logistics"
+admin.site.index_title = "Manage BitiCred Speed Logistics"
